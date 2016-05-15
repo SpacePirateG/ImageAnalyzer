6687a9fe65890592e7ac40669ea97c23556bf791
SpacePirateG <Vik-dv@yandex.ru>
2016-05-16 02:00:18 +0300

    added maping mapClass

--- ImageAnalyzerGUI/Storage.cs
+++ ImageAnalyzerGUI/Storage.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using ImageAnalyzer;
+using MongoDB.Bson.Serialization;
 
 namespace ImageAnalyzerGUI {
 	class Storage {
@@ -15,10 +16,26 @@ namespace ImageAnalyzerGUI {
 
 		public Storage () {
 
+			initClassMaps();
+
 			_client = new MongoClient();
 			_database = _client.GetDatabase("work");
 			_profiles = _database.GetCollection<Profile>("profiles");
 			_modules = _database.GetCollection<Module>("modules");
+		}
+
+		private void initClassMaps () {
+			BsonClassMap.RegisterClassMap<Profile>(cm => {
+				cm.AutoMap();
+				cm.MapMember(c => c.State).SetElementName("state");
+				cm.MapMember(c => c.Url).SetElementName("url");
+			});
+
+
+			BsonClassMap.RegisterClassMap<Module>(cm => {
+				cm.AutoMap();
+				cm.MapMember(c => c.Path).SetElementName("path");
+			});
 		}
 
 		public void AddModule (Module module) {

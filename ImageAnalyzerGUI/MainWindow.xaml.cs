7dabb5ac7f261bf7bd48de17d61b523a3a306514
SpacePirateG <Vik-dv@yandex.ru>
2016-05-15 19:19:45 +0300

    added handler start crawler and analyzer

--- ImageAnalyzerGUI/MainWindow.xaml.cs
+++ ImageAnalyzerGUI/MainWindow.xaml.cs
@@ -12,14 +12,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Configuration;
 
 namespace ImageAnalyzerGUI {
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		private Process _crawler;
+		private Process _analyzer;
 		public MainWindow () {
 			InitializeComponent();
+		}
+
+		private void Browse_Click (object sender, RoutedEventArgs e) {
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+			dlg.DefaultExt = ".dll";
+			dlg.Filter = "DLL Files|*.dll";
+
+			Nullable<bool> result = dlg.ShowDialog();
+
+			if (result == true) {
+				ModulePath.Text = dlg.FileName;
+			}
+		}
+
+		private void Start_Click (object sender, RoutedEventArgs e) {
+			_crawler = new Process();
+
+			_crawler.StartInfo.FileName = "node.exe";
+			_crawler.StartInfo.Arguments = ConfigurationManager.AppSettings["crawlerPath"];
+			_crawler.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+			_crawler.Start();
+
+			_analyzer = new Process();
+
+			_crawler.StartInfo.FileName = ConfigurationManager.AppSettings["analyzerPath"];
+			_crawler.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+			_crawler.Start();
+		}
+
+		private void AddPModule_Click (object sender, RoutedEventArgs e) {
+
+		}
+
+		private void AddProfiles_Click (object sender, RoutedEventArgs e) {
+
 		}
 	}
 }
